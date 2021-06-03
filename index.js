b57aec8c0794c7cc30baf45120c462f93a217e3f
Sagan <saganjfoster@gmail.com>
2021-06-03 03:46:39 +0000

    added server side pattern matching in the index.js file in order to ensure created resources are valid

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.put('/admin/category', (req, res) =>{
   getCategories().then(rows => {
     res.send(rows);
   });
-});
+})
 
 
 /***************************************************
@@ -103,8 +103,69 @@ app.put('/admin/category', (req, res) =>{
 
 app.post('/admin/resource', (req, res) => {
   const db = new sqlite3.Database('not911.db');
+  function checkField (pattern, field, message) {
+    if(!pattern.test(field)) {
+      res.status(400).send(message);
+      db.close();
+      res.end();
+      
+      return true;
+    }
+    return false;
+  }
 
   let {resource, phoneNumber, category, city, state} = req.body;
+
+  let reResource = /^[a-zA-Z\.\-'\s,0-9]{3,50}$/;
+  let rePhoneNumber = /^\(\d{3}\)\s*\d{3}-\d{4}$/;
+  let reCategory = /^[0-9]+$/;
+  let reCity = /^[a-zA-Z\.\-'\s,]{1,50}$/;
+  let reState = /^[a-zA-Z]{2}$/;
+
+  if(
+    checkField(reResource, resource, 'Resource name must be 3-100 characters') ||
+    checkField(rePhoneNumber, phoneNumber, 'Phone number must follow the formatting "(111) 222-3333"') ||
+    checkField(reCategory, category, 'Please select a valid category') ||
+    checkField(reCity, city, 'City names can only contain the characters a-z, symbols, amd must be 1-50 characters in length') ||
+    checkField(reState, state, 'Please enter a 2 letter state')
+  ) {
+    return;
+  }
+
+  /*if(!reResource.test(resource)){
+    res.status(400).send('Resource name must be 3-100 characters');
+    db.close();
+    res.end()
+    return;
+  }
+
+  if (!rePhoneNumber.test(phoneNumber)) {
+    res.status(400).send('Phone number must follow the formatting "(111) 222-3333"');
+    db.close();
+    res.end()
+    return;
+  }
+
+  if (!reCategory.test(category)) {
+    res.status(400).send('Please select a valid category');
+    db.close();
+    res.end()
+    return;
+  }
+
+  if (!reCity.test(city)) {
+    res.status(400).send('City names can only contain characters a-z, symbols, and must be 1-50 characters in length');
+    db.close();
+    res.end()
+    return;
+  }
+
+  if (!reState.test(state)) {
+    res.status(400).send('Please enter a 2 letter state');
+    db.close();
+    res.end()
+    return;
+  }*/
 
   db.serialize( () => {
     let insert = db.prepare('INSERT INTO resources (name, phone_number, catagory, city, state) VALUES (?, ?, ?, ?, ?)');
